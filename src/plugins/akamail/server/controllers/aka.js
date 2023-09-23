5ee374da7ba07bea63d0ac9b97fd4e793c474f47
s3713026 <phucuong200297@email.com>
2023-09-24 00:45:17 +0700

    delete

--- src/plugins/akamail/server/controllers/aka.js
+++ src/plugins/akamail/server/controllers/aka.js
@@ -68,7 +68,7 @@ module.exports = {
         let id = ''
         let i = 0;
         console.log(data_body);
-        console.log(user_identities.length);
+        console.log(user_identities[0].identity_type);
         for (i = 0;i <= user_identities.length;i++){
             if(user_identities[i].identity_type == 'email'){
                 Email = user_identities[i].identity 
