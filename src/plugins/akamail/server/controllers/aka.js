6d5b1cb8d2cf59a452f8b6fc831d8370f417fc8b
s3713026 <phucuong200297@email.com>
2023-09-23 12:33:10 +0700

    change negative

--- src/plugins/akamail/server/controllers/aka.js
+++ src/plugins/akamail/server/controllers/aka.js
@@ -42,7 +42,7 @@ module.exports = {
             let Email = user_identities[0].identity
             let Mobile = user_attributes.Mobile
             let City = user_attributes.City
-            console.log(data_body.mpid, Email,Mobile,City)
+            console.log("NUMM",data_body.mpid * -1)
             let count = 0;
             if(data_body.events[0].data.custom_event_type == 'add_to_cart'){
                 console.log('update')
