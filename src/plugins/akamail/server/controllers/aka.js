3895f8ef999769873597aeed2afc9b9cf44e1d72
s3713026 <phucuong200297@email.com>
2023-09-23 13:00:33 +0700

    change negative

--- src/plugins/akamail/server/controllers/aka.js
+++ src/plugins/akamail/server/controllers/aka.js
@@ -1,5 +1,32 @@
 const bcrypt = require("bcryptjs");
 
+function sendmail(email) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "domain": "app.akadigital.net",
+        "key": "aka",
+        "pkey": "private_app.key",
+        "from": "sender@app.akadigital.net",
+        "to": email,
+        "subject": "Test Mail",
+        "text": "This is a test email from app.",
+        "html": "<html><body><h1>Your HTML content here</h1></body></html>"
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("https://email.unito.vn/api/send-email", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
 
 module.exports = {
     async send(ctx) {
@@ -22,30 +49,30 @@ module.exports = {
 
         //if username - password match
         // if (check == true) {
-            console.log('run webhook')
-            ctx.body = "Welcome to AKA Netcore Webhook"
-            
+        console.log('run webhook')
+        ctx.body = "Welcome to AKA Netcore Webhook"
 
 
-            let request_urls = ctx.request.url;
-            let request_method = ctx.request.method;
-            let request_record = ctx.request["x-forwarded-for"];
+
+        let request_urls = ctx.request.url;
+        let request_method = ctx.request.method;
+        let request_record = ctx.request["x-forwarded-for"];
 
 
-            //string
-            let data_body = ctx.request.body;
-            let user_identities = data_body.user_identities;
-            let user_attributes = data_body.user_attributes;
-            let Email = user_identities[0].identity
-            let Mobile = user_attributes.Mobile
-            let City = user_attributes.City
-            console.log("NUMM",data_body.mpid * -1)
-            let count = 0;
-            if(data_body.events[0].data.custom_event_type == 'add_to_cart'){
-                console.log('update')
-                count = count +1;
-                let entry = await strapi.db.query('plugin::akamail.akalead').create({
-                    data: {
+        //string
+        let data_body = ctx.request.body;
+        let user_identities = data_body.user_identities;
+        let user_attributes = data_body.user_attributes;
+        let Email = user_identities[0].identity
+        let Mobile = user_attributes.Mobile
+        let City = user_attributes.City
+        console.log("NUMM", data_body.mpid * -1)
+        let count = 0;
+        if (data_body.events[0].data.custom_event_type == 'add_to_cart') {
+            console.log('update')
+            count = count + 1;
+            let entry = await strapi.db.query('plugin::akamail.akalead').create({
+                data: {
                     id: data_body.mpid * -1,
                     Email: Email,
                     Mobile: Mobile,
@@ -55,10 +82,11 @@ module.exports = {
                     //   Submitted_Date: Submitted_Date,
                     //   Journey_Name: journey_name,
                     //   List_Name: list_name
-                    }
-                });
-            }
-            console.log(count)
+                }
+            });
+            sendmail(Email)
+        }
+        console.log(count)
 
 
 
